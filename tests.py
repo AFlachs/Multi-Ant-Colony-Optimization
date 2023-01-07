434355876b2a33d6dc25654197abec19d68fb27a
jeanneszp <jeanne.szpirer@ulb.be>
2023-01-07 11:41:43 +0100

    Analyse graph 3

--- tests.py
+++ tests.py
@@ -3,6 +3,39 @@ from AntColonyOptimizer import AntColonyOptimizer
 import numpy as np
 import GraphUtils as gu
 import time
+import rust_macs
+
+M=np.inf
+init_pheromones = 0.05
+beta = 2
+rho = 0.1
+source = 0
+cl_len = 5
+
+# Function to calculate the coefficient of variance for graph 3
+cv = lambda x: np.std(x, ddof=1) / np.mean(x) * 100
+
+
+graph3 = np.array([
+            #0 1 2 3 4 5 6 7 8 9 . 1 2 3 4 5
+            [0,1,M,M,M,5,M,M,5,M,M,1,M,M,M,M],#V0
+            [1,0,1,M,M,M,M,M,M,M,M,M,M,M,M,M],#V1
+            [M,1,0,1,M,M,1,M,M,M,M,M,M,M,M,M],#V2
+            [M,M,1,0,2,3,M,M,M,M,M,M,M,M,M,M],#V3
+            [M,M,M,2,0,M,M,M,M,M,M,M,M,M,M,2],#V4
+            [5,M,M,3,M,0,1,M,M,6,M,M,M,M,M,M],#V5
+            [M,M,1,M,M,1,0,1,6,M,M,M,M,M,M,M],#V6
+            [M,M,M,M,M,M,1,0,M,M,M,M,M,M,M,1],#V7
+            [5,M,M,M,M,M,6,M,0,1,M,M,M,3,M,M],#V8
+            [M,M,M,M,M,6,M,M,1,0,1,M,1,M,M,M],#V9
+            [M,M,M,M,M,M,M,M,M,1,0,M,M,M,M,1],#V10
+            [1,M,M,M,M,M,M,M,M,M,M,0,1,M,M,M],#V11
+            [M,M,M,M,M,M,M,M,M,1,M,1,0,1,M,M],#V12
+            [M,M,M,M,M,M,M,M,3,M,M,M,1,0,2,M],#V13
+            [M,M,M,M,M,M,M,M,M,M,M,M,M,2,0,2],#V14
+            [M,M,M,M,2,M,M,1,M,M,1,M,M,M,2,0]#V15
+        ])
+
 
 def seq_in_list(list, seq):
     for i in range(len(list) - len(seq) + 1):
@@ -118,19 +151,27 @@ def test_paper_graph_2_weight_method1():
 
 
 def test_paper_graph3():
+    n_ants = 12
+    gamma = 2
+    dest = 15
+    t_max = 1000 # Tah Jul
     f = open("Files/graph3_paper.csv", "a")
     writer_csv = writer(f)
     nb_types = [2, 3, 4]
     q_0_list = np.arange(0, 1, 0.1)
     for nb_type in nb_types:
         for q_0 in q_0_list:
-            nb_disjoint = 0
+            min_length_per_run = []
             for i in range(100):
-                optimizer = AntColonyOptimizer(ants=12, types=nb_type, init_pheromones=0.05, beta=2, choose_best=q_0,
-                                               gamma=2, rho=0.1)
-                best = optimizer.fit(4, 1000, verbose=False)
-                if check_disjoints(best):
-                    nb_disjoint+=1
+                result = rust_macs.optimize_macs(graph3, n_ants, nb_type, init_pheromones, beta, q_0, gamma, rho, source, dest, t_max, cl_len)
+                result_paths = [i[0] for i in result]
+                result_lengths = [i[2] for i in result]
+                if check_disjoints(result_paths):
+                    min_length_per_run.append(min(result_lengths))
+            optimal_sol = min(min_length_per_run)
+            perc_opt = min_length_per_run.count(optimal_sol)/len(min_length_per_run) #On compte combien de runs disjoints contiennent la meilleure solution
+            coef_var = cv(min_length_per_run)
+            writer_csv.writerow([nb_type,q_0,optimal_sol,len(min_length_per_run),perc_opt,coef_var])
 
 
 def check_disjoints(best):
@@ -144,59 +185,60 @@ def check_disjoints(best):
     return True
 
 
-
-
 def test_barbell_modified(verbose=True):
-    #VARIABLES (for tests)
-    LIST_NODES = [10,15,20,25,30]#TODO: should be [10, 50, 100, 500]
+    # VARIABLES (for tests)
+    LIST_NODES = [10, 15, 20, 25, 30]  # TODO: should be [10, 50, 100, 500]
     N_LINKS = 3
-    LIST_N_ANT_TYPES = [2,3,4] #TODO: AT LEAST 2 ANT TYPES (1 causes bug)
-    N_PAIRS = 1 #TODO: should be 5, but would be identical in case of Barbell...
-    N_GRAPHS = 1 #TODO: should be 3, but would be identical in case of Barbell...
+    LIST_N_ANT_TYPES = [2, 3, 4]  # TODO: AT LEAST 2 ANT TYPES (1 causes bug)
+    N_PAIRS = 1  # TODO: should be 5, but would be identical in case of Barbell...
+    N_GRAPHS = 1  # TODO: should be 3, but would be identical in case of Barbell...
 
-    #CONSTANTS (from optimizer)
+    # CONSTANTS (from optimizer)
     N_TESTS = 50
-    ANT_NUMBER=10
+    ANT_NUMBER = 10
     q_0, g = 0, 1
 
-    filename="barbell_001"
-    f = open("Files/%s.csv"%(filename),'a',newline='')
+    filename = "barbell_001"
+    f = open("Files/%s.csv" % (filename), 'a', newline='')
     ww = writer(f)
-    ww.writerow(["# nodes","# links","no graph","# ant types","% success/{:d}tests".format(N_TESTS),"time(s)"])
-    tic=time.perf_counter()
-    itic=tic
+    ww.writerow(["# nodes", "# links", "no graph", "# ant types", "% success/{:d}tests".format(N_TESTS), "time(s)"])
+    tic = time.perf_counter()
+    itic = tic
     for nnodes in LIST_NODES:
         for ing in range(N_GRAPHS):
-            if verbose:print("Initialization of Barbell graph n° ",ing," with ",nnodes," nodes and " ,N_LINKS," bridges...")
-            graph = gu.GraphFactory_barbell(nnodes,N_LINKS,ret_type='mat')
+            if verbose: print("Initialization of Barbell graph n° ", ing, " with ", nnodes, " nodes and ", N_LINKS,
+                              " bridges...")
+            graph = gu.GraphFactory_barbell(nnodes, N_LINKS, ret_type='mat')
             best_paths = gu.get_all_disjoint_paths(graph)
             for ntypes in LIST_N_ANT_TYPES:
-                if verbose:print("  Tests with ",ntypes," ant types")
-                #nb_type=nlinks #the number of types of ants is the number of link for barbel
-                mincut=N_LINKS#same for the value of the minimum cut
+                if verbose: print("  Tests with ", ntypes, " ant types")
+                # nb_type=nlinks #the number of types of ants is the number of link for barbel
+                mincut = N_LINKS  # same for the value of the minimum cut
                 sum_successes = 0
                 for _ in range(N_TESTS):
-                    if verbose:print("    :test",_,": began...")
-                    optimizer = AntColonyOptimizer(ants=ANT_NUMBER, types=ntypes, init_pheromones=0.05, beta=2, choose_best=q_0,
-                                               gamma=g, rho=0.1)
-                    best = optimizer.fit(0,graph_mat=graph,iterations=100, verbose=False,verbis=False)
-                    i=0
+                    if verbose: print("    :test", _, ": began...")
+                    optimizer = AntColonyOptimizer(ants=ANT_NUMBER, types=ntypes, init_pheromones=0.05, beta=2,
+                                                   choose_best=q_0,
+                                                   gamma=g, rho=0.1)
+                    best = optimizer.fit(0, graph_mat=graph, iterations=100, verbose=False, verbis=False)
+                    i = 0
                     for j in range(len(best)):
                         if best[j] in best_paths:
-                            i+=1
-                    if i==len(best):
-                        if verbose:print("    :test",_,": succeeded !\n     sol=",best)
-                        sum_successes+=1
+                            i += 1
+                    if i == len(best):
+                        if verbose: print("    :test", _, ": succeeded !\n     sol=", best)
+                        sum_successes += 1
                     else:
-                        if verbose:print("    :test",_,": failed !\n     sol=",best)
-                #CSV    #nodes,#links,n°graph,#types,%
-                tac=time.perf_counter()
-                ww.writerow([nnodes,N_LINKS,ing,ntypes,sum_successes*(100/N_TESTS),round(tac-itic,2)])
-                itic=tac
-                
+                        if verbose: print("    :test", _, ": failed !\n     sol=", best)
+                # CSV    #nodes,#links,n°graph,#types,%
+                tac = time.perf_counter()
+                ww.writerow([nnodes, N_LINKS, ing, ntypes, sum_successes * (100 / N_TESTS), round(tac - itic, 2)])
+                itic = tac
+
     f.close()
-    tac=time.perf_counter()
+    tac = time.perf_counter()
     print(f"Barbell took {tac - tic:0.1f} seconds to run.")
+
 
 def test_small_world():
     """
@@ -211,22 +253,23 @@ def test_scale_free():
     """
     pass
 
-def test_freq_opt(num_graph,gamma,q0,iters=100):
+
+def test_freq_opt(num_graph, gamma, q0, iters=100):
     sum = 0
     for _ in range(iters):
         optimizer = AntColonyOptimizer(ants=5, types=2, init_pheromones=0.05, alpha=1, beta=2,
-                                    beta_evaporation_rate=0, choose_best=q0, gamma=gamma, rho=0.1)
+                                       beta_evaporation_rate=0, choose_best=q0, gamma=gamma, rho=0.1)
         best = optimizer.fit(num_graph, 20, verbose=False)
-        if num_graph==1 and best == [[0, 2, 3], [0, 1, 3]] or best == [[0, 1, 3], [0, 2, 3]]:
+        if num_graph == 1 and best == [[0, 2, 3], [0, 1, 3]] or best == [[0, 1, 3], [0, 2, 3]]:
             sum += 1
-        elif num_graph==2 and True:
+        elif num_graph == 2 and True:
             sum += 1
             raise NotImplementedError
-        elif num_graph==3 and True:
+        elif num_graph == 3 and True:
             sum += 1
             raise NotImplementedError
     return sum
-        
+
 
 def test_freq_bridge_passages():
     """
@@ -236,16 +279,17 @@ def test_freq_bridge_passages():
 
 
 def test_paper(num_graph):
-    Qs=[i*0.1 for i in range(0,6)]
-    gammas=[i*0.1 for i in range(0,10)]
-    res = np.ndarray(shape=(len(gammas),len(Qs)))
+    Qs = [i * 0.1 for i in range(0, 6)]
+    gammas = [i * 0.1 for i in range(0, 10)]
+    res = np.ndarray(shape=(len(gammas), len(Qs)))
 
-    for ig,g in enumerate(gammas):
-        for iq,q in enumerate(Qs):
-            res[ig,iq]=test_freq_opt(num_graph,g,q)
-    #header are not writed in the .csv (g's\q's)->(vertical\horizontal) 
-    np.savetxt("test_paper_graph%d.csv",res,fmt="%d",delimiter=",")
+    for ig, g in enumerate(gammas):
+        for iq, q in enumerate(Qs):
+            res[ig, iq] = test_freq_opt(num_graph, g, q)
+    # header are not writed in the .csv (g's\q's)->(vertical\horizontal)
+    np.savetxt("test_paper_graph%d.csv", res, fmt="%d", delimiter=",")
+
 
 if __name__ == '__main__':
-    #test_paper_graph1()
+    # test_paper_graph1()
     test_barbell_modified()
